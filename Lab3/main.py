786c89811f72715f10dd2f1ca58355eb058b15f7
Nicolás Gutiérrez <nicolas.gutierrez.p@usach.cl>
2020-09-23 04:54:25 -0300

    ADD: lab3: cipher

---
+++ Lab3/main.py
@@ -0,0 +1,20 @@
+
+
+##################################
+# Lab 3: Criptografia
+##################################
+
+'''
+En este laboratorio se implementará el cifrado simetrico de HILL, este cifrado consiste en definir 
+un alfabeto el cual puede incluir caracteres especiales.
+
+
+'''
+
+
+ALPHABET = ["a", "b", "c", "d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",",","."," "]
+
+
+
+def hill_cipher(matriz_key,word):
+    
